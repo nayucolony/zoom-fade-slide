00ee8bc1236a6986d435de367ed1fb31ba91a861
yuri <yurixxx8@users.noreply.github.com>
2017-02-05 02:55:15 +0900

    Update slide.js
    Use variables for selector

--- zoom-fade-slide/js/slide.js
+++ zoom-fade-slide/js/slide.js
@@ -1,27 +1,27 @@
 $(window).load(function(){
 var slide = $(".slideshow");
 var i = 0;
-$(slide).find('.item').eq(i).fadeIn(2000).addClass('in');
+slide.find('.item').eq(i).fadeIn(2000).addClass('in');
 var total = $(".slideshow .item").length -1;
 setInterval(function(){
   if(i < total){
-  $(slide).find('.item').eq(i).addClass('out');
-  $(slide).find('.item').eq(i).removeClass('in');
+  slide.find('.item').eq(i).addClass('out');
+  slide.find('.item').eq(i).removeClass('in');
     j = i;
     i++;
     setTimeout(function(){
-      $(slide).find('.item').eq(i).fadeIn(3500).addClass('in').removeClass('out');
+      slide.find('.item').eq(i).fadeIn(3500).addClass('in').removeClass('out');
     },1000);
-    $(slide).find('.item').eq(j).fadeOut(4500);
+    slide.find('.item').eq(j).fadeOut(4500);
   } else if(i == total){
-  $(slide).find('.item').eq(i).addClass('out');
-  $(slide).find('.item').eq(i).removeClass('in');
+  slide.find('.item').eq(i).addClass('out');
+  slide.find('.item').eq(i).removeClass('in');
     j = i;
     i = 0;
     setTimeout(function(){
-      $(slide).find('.item').eq(i).fadeIn(3500).addClass('in').removeClass('out');
+      slide.find('.item').eq(i).fadeIn(3500).addClass('in').removeClass('out');
     },1000);
-      $(slide).find('.item').eq(j).fadeOut(4500);
+    slide.find('.item').eq(j).fadeOut(4500);
   };
 },4500);
 });
