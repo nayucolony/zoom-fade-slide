7695c7495275f22a9a9ade39eaac19cf54597345
yuri <yurixxx8@users.noreply.github.com>
2017-02-05 00:41:11 +0900

    slide.js
    Delete comment

--- zoom-fade-slide/js/slide.js
+++ zoom-fade-slide/js/slide.js
@@ -1,11 +1,8 @@
 $(window).load(function(){
 var slide = $(".slideshow");
 var i = 0;
-// ページの読み込み時にスライドの1枚目を2000ミリ秒かけてフェードイン
 $(slide).find('.item').eq(i).fadeIn(2000).addClass('in');
-// スライドの枚数を調べて変数に格納
 var total = $(".slideshow .item").length -1;
-// 処理を繰り返す
 setInterval(function(){
   if(i < total){
   $(slide).find('.item').eq(i).addClass('out');
@@ -13,10 +10,8 @@ setInterval(function(){
     j = i;
     i++;
     setTimeout(function(){
-      // 1000ミリ秒後に3500ミリ秒かけてフェードインする
       $(slide).find('.item').eq(i).fadeIn(3500).addClass('in').removeClass('out');
     },1000);
-    // 4500ミリ秒かけてフェードアウトする
     $(slide).find('.item').eq(j).fadeOut(4500);
   } else if(i == total){
   $(slide).find('.item').eq(i).addClass('out');
@@ -28,6 +23,5 @@ setInterval(function(){
     },1000);
       $(slide).find('.item').eq(j).fadeOut(4500);
   };
-// ここを増やすと停滞時間を変えられる
 },4500);
 });
